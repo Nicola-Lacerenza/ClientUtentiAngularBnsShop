ea9d8c14c2850ac9740ae41562204ce78a817044
Nicola Lacerenza <nicolace98gmail.com>
2025-03-16 12:48:48 +0100

    UPDATE NL 16/03/2025 13:00

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,33 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { AuthappService } from '../../../services/authapp.service';
+import { user } from './../../../models/user';
 import { UserRegister } from '../../../models/UserRegister.interface';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']  // Usa styleUrls (con la "s")
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
-  public utente!: UserRegister;
-  public adminUser: boolean = false; // Proprietà modificabile per il ruolo admin
-  public loggedIn: boolean = false;  // Proprietà per lo stato di login
 
-  constructor(public basicAuth: AuthappService) {}
+  private _utente !: UserRegister;
+  private _isAdmin : boolean;
 
-  ngOnInit(): void {
-    // Imposta lo stato di login all'avvio
-    this.loggedIn = this.basicAuth.isLogged();
+  constructor(public BasicAuth: AuthappService){
+    this._isAdmin=false;
+  }
 
-    // Sottoscrivi il risultato dell'osservabile per l'admin
-    this.basicAuth.isAdmin().subscribe({
-      next: (ruolo: boolean) => {
-        this.adminUser = ruolo;
+  public get utente():UserRegister{
+    return this._utente;
+  }
+  
+  ngOnInit(): void {}
+
+  public isLogged():boolean{
+    return this.BasicAuth.isLogged();
+  }
+
+  public get isAdmin(): boolean{
+    this.BasicAuth.isAdmin().subscribe({
+      next:(ruolo:boolean)=>{
+        this._isAdmin=ruolo;
       },
-      error: (error: HttpErrorResponse) => {
+      error:(error:HttpErrorResponse)=>{
+        this._isAdmin=false;
         console.error(error);
-        this.adminUser = false;
       }
     });
+    return this._isAdmin;
   }
 }
